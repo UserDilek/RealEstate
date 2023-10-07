39c790c6d0f109c9e0ae45162851b315fbcaf8be
Dilek Hocaoglu <dilekhocaoglu11@hotmail.com>
2023-10-07 11:41:47 -0400

    env

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect(
-  "mongodb+srv://admin:admin@mere-estate.iaizvpx.mongodb.net/mern-estate?retryWrites=true&w=majority"
-);
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
