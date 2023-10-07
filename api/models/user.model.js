bb4629d33284e41d15d1d7c84f5d515668c4814e
Dilek Hocaoglu <dilekhocaoglu11@hotmail.com>
2023-10-07 12:53:44 -0400

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      uniquie: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      uniquie: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      uniquie: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
